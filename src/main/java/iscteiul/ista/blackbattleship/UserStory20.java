db251c7f3c9f0b1c64f531b9b5062d0fb6e1303a
IGE-110323 <fmsao2@iscte-iul.pt>
2026-05-15 14:17:51 +0100

    Rename test classes and packages to align with exercise structure.

--- src/main/java/iscteiul/ista/blackbattleship/UserStory20.java
+++ src/main/java/iscteiul/ista/blackbattleship/UserStory20.java
@@ -1,11 +1,6 @@
 package iscteiul.ista.blackbattleship;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
